44263a250d670c3c1272b094287d6396dfe70b46
Leonardo Casagrande <l.casagrande@feefo.com>
2023-05-14 11:15:27 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv-safe').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use('/', (req, res, next) => {
+    console.log(req.body);
+    res.send('Hello World');
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running in port ${PORT}`)
+})
